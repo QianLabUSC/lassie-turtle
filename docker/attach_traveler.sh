7459e5e5a09e789818f80e0233d30888540ea459
Ethan Fulcher <efulcher@usc.edu>
2023-11-10 15:37:05 -0800

    delete unnecessary files

--- docker/attach_traveler.sh
+++
@@ -1,1 +0,0 @@
-sudo docker exec -it traveler_container bash
--- docker/start_traveler_with_control.sh
+++
@@ -1,2 +0,0 @@
-sudo docker start traveler_container
-sudo docker exec -it traveler_container bash -c "source /opt/ros/humble/setup.bash && source /home/traveler/dependencies/install/setup.bash && export ROS_DOMAIN_ID=19 && python3 /home/traveler/highlevel/rasp_main.py"
