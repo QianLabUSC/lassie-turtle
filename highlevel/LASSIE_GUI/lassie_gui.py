c8277ecf6771e0424fdc9d5383badfabba974595
Ethan Fulcher <efulcher@usc.edu>
2025-03-28 14:05:55 -0700

    radian to degree conversion

--- highlevel/LASSIE_GUI/lassie_gui.py
+++ highlevel/LASSIE_GUI/lassie_gui.py
@@ -224,14 +224,14 @@ class TravelerApp(MDApp):
         self.current_tab.ids.Optimize_Variable_6.text = str(round(self.current_tab.ids.Slider_optimize_6.value))
 
     def on_change_Optimize_Variable_7(self):
-        self.current_tab.ids.Optimize_Variable_7.text = str(round(self.current_tab.ids.Slider_optimize_7.value))
+        self.current_tab.ids.Optimize_Variable_7.text = str((self.current_tab.ids.Slider_optimize_7.value))
 
     def on_change_Optimize_Variable_8(self):
-        self.current_tab.ids.Optimize_Variable_8.text = str(round(self.current_tab.ids.Slider_optimize_8.value))
+        self.current_tab.ids.Optimize_Variable_8.text = str((self.current_tab.ids.Slider_optimize_8.value))
     def on_change_Optimize_Variable_9(self):
-        self.current_tab.ids.Optimize_Variable_9.text = str(round(self.current_tab.ids.Slider_optimize_9.value))
+        self.current_tab.ids.Optimize_Variable_9.text = str((self.current_tab.ids.Slider_optimize_9.value))
     def on_change_Optimize_Variable_10(self):
-        self.current_tab.ids.Optimize_Variable_10.text = str(round(self.current_tab.ids.Slider_optimize_10.value))
+        self.current_tab.ids.Optimize_Variable_10.text = str((self.current_tab.ids.Slider_optimize_10.value))
 
 
 
@@ -359,10 +359,10 @@ class TravelerApp(MDApp):
                 self.gui_message.data.append(float(round(self.turtle_optimize_tab.ids.Slider_optimize_2.value))/1000 )    
                 self.gui_message.data.append(float(round(self.turtle_optimize_tab.ids.Slider_optimize_1.value)) * np.pi / 180  )     
                 # MODIFIED: Append new start and end coordinate parameters (converted to radians)
-                self.gui_message.data.append(float(round(self.turtle_optimize_tab.ids.Slider_optimize_7.value)) * np.pi / 180)  # MODIFIED: start gamma
-                self.gui_message.data.append(float(round(self.turtle_optimize_tab.ids.Slider_optimize_8.value)) * np.pi / 180)  # MODIFIED: start theta
-                self.gui_message.data.append(float(round(self.turtle_optimize_tab.ids.Slider_optimize_9.value)) * np.pi / 180)  # MODIFIED: end gamma
-                self.gui_message.data.append(float(round(self.turtle_optimize_tab.ids.Slider_optimize_10.value)) * np.pi / 180) # MODIFIED: end theta
+                self.gui_message.data.append(float(round(self.turtle_optimize_tab.ids.Slider_optimize_7.value, 2)) * np.pi / 180)  # MODIFIED: start gamma
+                self.gui_message.data.append(float(round(self.turtle_optimize_tab.ids.Slider_optimize_8.value, 2)) * np.pi / 180)  # MODIFIED: start theta
+                self.gui_message.data.append(float(round(self.turtle_optimize_tab.ids.Slider_optimize_9.value, 2)) * np.pi / 180)  # MODIFIED: end gamma
+                self.gui_message.data.append(float(round(self.turtle_optimize_tab.ids.Slider_optimize_10.value, 2)) * np.pi / 180) # MODIFIED: end theta
 
 
          
